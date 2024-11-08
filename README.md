96135df3b8a32c514a938e7a6b30fe737330ef9e
ykskb <yoheikusakabe@gmail.com>
2024-11-08 16:13:06 +0800

    Init commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Duckie Playground
+
+Minimal (single-file) web service in Go for SQL (`SELECT`) playground with DuckDB 
+
+### Background
+
+DuckDB WASM is awesome. But sometimes it's not easy to make data files accessible to your local (like S3 in enterprise).
+
+So I made a very simple web service in Go that hosts data and runs SQL through DuckDB.
+
+### Minimal usage by trusted users only
+
+* Not perfectly secure:
+
+    The service validates the names of data sources and checks only `SELECT` statement can run, but it does not intend to be perfectly secure. 
+
+* Scalability unknown:
+
+    Each request creates DuckDB instance within goroutine, but scalability is unknown. I'd imagine heavy queries can easily use up RAM and CPUs, depending on workloads.
+
+* No Web API:
+
+    For the reasons above, only UI-based usage is supported for human usage.
+
+* No CSS:
+
+    Only raw HTML tags are used. Sorry.
+
+### How to use
+
+1. Multiple data source can be configured. Basically it's a list of strings after `FROM` clause, so CSV file, Parquet file or functions like `read_parquet` can be used, including partitioned data. Just update `dataSources` string array in `duck-server.go` and make sure it's accessible from root.
+
+2. Run `go run duckie.go` to start the service.
+
+3. Visit the UI at `127.0.0.1:8081`
+
+4. Selecting data source automatically runs `DESCRIBE` and shows data source schema.
+
+5. Run any `SELECT` query you want to.
---
+++ duckie.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"reflect"
+	"regexp"
+	"slices"
+
+	"github.com/xwb1989/sqlparser"
+
+	_ "github.com/marcboeker/go-duckdb"
+)
+
+type Record []interface{}
+
+type Page struct {
+	SelectedSource string
+	DataSources    []string
+	Query          string
+	Results        []Record
+	Columns        []string
+}
+
+var dataSources = []string{"call_center.csv", "catalog_page.csv"}
+
+var describeColumns = []string{"column_name", "column_type", "null", "key", "default", "extra"}
+
+func runQuery(query string, sel_count int) ([]Record, error) {
+	db, err := sql.Open("duckdb", "?threads=4")
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	results := []Record{}
+	for rows.Next() {
+		values := make([]interface{}, sel_count)
+		for i := range values {
+			values[i] = new(interface{})
+		}
+		rows.Scan(values...)
+		results = append(results, values)
+	}
+	return results, nil
+}
+
+func sourceExists(selection string) bool {
+	return slices.Contains(dataSources, selection)
+}
+
+func describeSource(dataSource string) ([]Record, error) {
+	query := fmt.Sprintf("DESCRIBE SELECT * FROM %s", dataSource)
+	results, err := runQuery(query, len(describeColumns))
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+func initPage(selectedSource string) (*Page, error) {
+	if len(selectedSource) > 0 && !sourceExists(selectedSource) {
+		return nil, errors.New("selected data source does not exist")
+	}
+	return &Page{SelectedSource: selectedSource, DataSources: dataSources}, nil
+}
+
+func queryPageHandler(w http.ResponseWriter, r *http.Request, title string) {
+	dataSource := r.URL.Query().Get("datasource")
+	page, err := initPage(dataSource)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if len(dataSource) > 0 {
+		page.Query = fmt.Sprintf("DESCRIBE SELECT * FROM %s", dataSource)
+		results, err := describeSource(dataSource)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		}
+		page.Results = results
+	} else {
+		page.Query = "Please choose data source from above."
+	}
+	page.Columns = describeColumns
+	renderTemplate(w, "query", page)
+}
+
+func queryHandler(w http.ResponseWriter, r *http.Request, title string) {
+	selectedSource := r.FormValue("datasource")
+	page, err := initPage(selectedSource)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	q := r.FormValue("query")
+	stmt, err := sqlparser.Parse(q)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	select_stmt, ok := stmt.(*sqlparser.Select)
+	if !ok {
+		http.Error(w, "Not a SELECT statement.", http.StatusInternalServerError)
+		return
+	}
+
+	columns := []string{}
+	for _, expr := range select_stmt.SelectExprs {
+		switch col := expr.(type) {
+		case *sqlparser.AliasedExpr:
+			// col.Expr is the actual expression (e.g., column name or function call)
+			if colName, ok := col.Expr.(*sqlparser.ColName); ok {
+				columns = append(columns, colName.Name.String())
+			} else {
+				// Handle expressions like COUNT(*), CONCAT(name, last_name), etc.
+				columns = append(columns, sqlparser.String(col.Expr))
+			}
+		case *sqlparser.StarExpr:
+			describeResults, err := describeSource(selectedSource)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			for _, describeEntry := range describeResults {
+				v := reflect.ValueOf(describeEntry[0]).Elem()
+				columns = append(columns, v.Interface().(string))
+			}
+		}
+	}
+
+	results, err := runQuery(q, len(columns))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	page.Query = q
+	page.Columns = columns
+	page.Results = results
+	page.SelectedSource = selectedSource
+
+	// API:
+	// w.Header().Set("Content-Type", "application/json")
+	// json.NewEncoder(w).Encode("{\"test\": \"success\"}")
+	renderTemplate(w, "query", page)
+}
+
+var templates = template.Must(template.ParseFiles("query.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var validPath = regexp.MustCompile("^(/|/query)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[0])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", makeHandler(queryPageHandler))
+	http.HandleFunc("/query", makeHandler(queryHandler))
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
